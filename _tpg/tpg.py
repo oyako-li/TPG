e289d5d5f011b29b41df77f38c052e40c9d4281a
oyako-li <oyakoli3@gmail.com>
2022-10-03 16:44:53 +0900

    update automata point test

--- _tpg/tpg.py
+++ _tpg/tpg.py
@@ -841,8 +841,10 @@ class Automata(_TPG):
         _task   = self.env.spec.id
         self.setAgents()
         # breakpoint(type(emulators))
+        print('start episode...')
         for _ in range(self.episodes):
             total_reward = self.episode()
+        print('... end episode')
 
         # 報酬の贈与
         for actor in self.actors:
